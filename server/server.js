ff0407dd5533225bfcfdabcf374720417a03ffee
Jakub Majdl <jkbmaj@gmail.com>
2017-03-17 16:28:04 +0100

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -43,7 +43,24 @@ app.get('/todos/:id', (req, res) => {
             return res.status(404).send();
         }
         res.send({todo});
-    }, (e) => {
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    let id = req.params.id;
+
+    if (!ObjectId.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
         res.status(400).send();
     });
 });
