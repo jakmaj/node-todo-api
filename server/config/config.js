e1aa9b3ce88e6b75026a5f10511c5734f2e02b78
Jakub Majdl <jkbmaj@gmail.com>
2017-04-09 11:42:01 +0200

    Add JWT_SECRET to config

--- server/config/config.js
+++ server/config/config.js
@@ -5,7 +5,9 @@ console.log('Server environment:', env);
 if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    process.env.JWT_SECRET = 'vnr3i54tnignjerngigi5gn5i';
 } else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    process.env.JWT_SECRET = 'e4knt5gmk34kgn73gnk4ng4ng';
 }
